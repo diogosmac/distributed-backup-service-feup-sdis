699c77ff863931ddf713102cb4cc79968556dabc
Diogo Machado <diogo.smac@gmail.com>
2020-02-25 11:04:08 +0000

    OOP, all operations and then some, only source code (repo structure)

--- lab01/src/Client.java
+++ lab01/src/Client.java
@@ -1,61 +1,51 @@
-package com.pedro.sdis;
-
 import java.io.IOException;
 import java.net.DatagramPacket;
 import java.net.DatagramSocket;
 import java.net.InetAddress;
+import java.util.Arrays;
 
 public class Client {
 
-    //args[0] => is the DNS name (or the IP address, in the dotted decimal format) where the server is running
-    //args[1] => is the port number where the server is providing service
-    //args[2] =>  is the operation to request from the server, either "register" or "lookup"
-    //args[>3] => is the list of operands of that operation
-                    //<DNS name> <IP address> for register
-                    //<DNS name> for lookup
+    // args[0] => is the DNS name (or the IP address, in the dotted decimal format) where the server is running
+    // args[1] => is the port number where the server is providing service
+    // args[2] =>  is the operation to request from the server, either "register" or "lookup"
+    // args[>3] => is the list of operands of that operation
+    // <DNS name> <IP address> for register
+    // <DNS name> for lookup
 
     public static void main(String[] args) throws IOException {
-        if(args.length != 4 && args.length != 5 ) {
+
+        if (args.length < 3) {
             System.out.println("Usage: Client <host> <port> <oper> <opnd>*");
             return;
         }
-        else {
-            System.out.println("Success!");
 
-            InetAddress address = InetAddress.getByName(args[0]);
-            int port = Integer.parseInt(args[1]);
+        InetAddress address = InetAddress.getByName(args[0]);
+        int port = Integer.parseInt(args[1]);
 
-//            InetAddress localHost = InetAddress.getLocalHost();
-            DatagramSocket socket = new DatagramSocket();
+        DatagramSocket socket = new DatagramSocket();
 
-            byte[] bytesToSend;
+        Request request = Request.fromArgs(
+                Arrays.copyOfRange(args, 2, args.length));
+        if (request == null) return;
 
-            if(args[2].equals("register")) {
+        byte[] bytesToSend;
+        bytesToSend = request.toString().getBytes();
 
-                if(args.length != 5) {
-                    System.out.println("Missing parameters");
-                    return;
-                }
-                String message = args[2] + args[3] + args[4];
-                 bytesToSend = message.getBytes();
-            }
-            else if(args[2].equals("lookup") ) {
-                String message = args[2] + args[3];
-                bytesToSend = message.getBytes();
-            }
-            else {
-                System.out.println("Invalid operation");
-                return;
-            }
+        DatagramPacket packet = new DatagramPacket(bytesToSend, bytesToSend.length, address, port);
 
-            DatagramPacket packet = new DatagramPacket(bytesToSend, bytesToSend.length, address, port);
+        System.out.println("\nSending request to:\t" + address.getHostAddress() + ":" + port);
+        System.out.println("Operation:\t\t" + request.toString() + "\n");
+        socket.send(packet);
 
-            socket.send(packet);
+        byte[] buff = new byte[Request.MAX_SIZE];
+        DatagramPacket response = new DatagramPacket(buff, Request.MAX_SIZE);
 
-            System.out.println("Sent?");
-            System.out.println("Porta: " + port);
-            System.out.println("Address: " + address);
-            System.out.println("Operation: " + args[2]);
-        }
+        socket.receive(response);
+
+        String received = new String(response.getData(), 0, response.getLength());
+        System.out.println("Client:\t" + request.toString() + " : " + received + "\n");
+
     }
+
 }
--- lab01/src/Server.java
+++ lab01/src/Server.java
@@ -1,12 +1,15 @@
 import java.io.IOException;
 import java.net.*;
+import java.util.HashMap;
+import java.util.concurrent.atomic.AtomicBoolean;
 
 public class Server {
 
     private final static int OK = 0;
     private final static int ERROR = -1;
-    private final static int MAX_SIZE = 256;
 
+    private static HashMap<String, String> dnsTable;
+    private static AtomicBoolean closeRequested = new AtomicBoolean(false);
 
     private static DatagramSocket socket;
 
@@ -19,48 +22,80 @@ public class Server {
             return ERROR;
         }
 
-//        try { socket = new DatagramSocket(port, inetAddress); }
         try { socket = new DatagramSocket(port); }
         catch (SocketException e) {
             System.out.println("Socket exception");
             return ERROR;
         }
 
-        System.out.println("\nSocket created:");
+        System.out.println("\nServer opened:");
         System.out.println("\tIP Address: " + inetAddress.getHostAddress());
-        System.out.println("\tPort: " + port);
-        System.out.println();
+        System.out.println("\tPort: " + port + "\n");
 
         return OK;
 
     }
 
-    public static void main(String[] args) {
+    private static String process(Request request) {
+
+        String[] params = request.getData();
+
+        switch(request.type) {
+            case "register":
+                if (dnsTable.containsKey(params[0])) { return "ALREADY_REGISTERED"; }
+                else {
+                    dnsTable.put(params[0], params[1]);
+                    return Integer.toString(dnsTable.size());
+                }
+            case "lookup":
+                return dnsTable.getOrDefault(params[0], "NOT_FOUND");
+            case "close":
+                closeRequested.set(true);
+                return "CLOSED_SUCCESSFULLY";
+            case "reset":
+                dnsTable.clear();
+                return "RESET_SUCCESSFULLY";
+            default: return "ERROR";
+        }
+
+    }
+
+    public static void main(String[] args) throws IOException {
 
         if (args.length != 1) {
             System.out.println("Usage: java Server <port number>");
             return;
         }
 
+        dnsTable = new HashMap<String, String>();
+
         int port = Integer.parseInt(args[0]);
         if (openSocket(port) != OK) {
             System.out.println("An error occurred while creating the server");
             return;
-        };
+        }
 
-        for (int i = 0; i < 5; i++) {
+        DatagramSocket responseSocket = new DatagramSocket();
+        while (!closeRequested.get()) {
 
-            byte[] buff = new byte[MAX_SIZE];
-            DatagramPacket packet = new DatagramPacket(buff, MAX_SIZE);
+            System.out.println("Waiting for request...");
+            byte[] buff = new byte[Request.MAX_SIZE];
 
-            try { socket.receive(packet); }
-            catch (IOException e) {
-                System.out.println("Exception on the received packet");
-                continue;
-            }
+            DatagramPacket packet = new DatagramPacket(buff, Request.MAX_SIZE);
+            socket.receive(packet);
+            String received = new String(packet.getData(), 0, packet.getLength());
+            System.out.println("Received:\n\t" + received + "\n");
 
-            int bytesReceived = packet.getLength();
-            System.out.println("Received " + bytesReceived + " bytes");
+            String[] arguments = received.split(Request.BREAK);
+            Request request = Request.fromArgs(arguments);
+            String reply = (request != null) ? process(request) : "ERROR";
+            byte[] bytesToSend = reply.getBytes();
+
+            DatagramPacket response = new DatagramPacket(
+                    bytesToSend, bytesToSend.length,
+                    packet.getAddress(), packet.getPort()
+            );
+            responseSocket.send(response);
 
         }
 
