2f781db0bb5ab4cb0d76d8be0b960dca1d80650d
Pedro Baptista <ze.pedro3622@outlook.pt>
2020-04-14 18:06:27 +0100

    Scripts added for launching peers, and for each Aplication function

---
+++ scripts/Restore.sh
@@ -0,0 +1,13 @@
+#!/usr/bin/env bash
+
+script_name=$0
+peer_id=$1
+file_path=$2
+
+if [ "$#" -ne 2 ]; then
+    echo "Usage: $script_name <peer_id> <file_path>"
+    exit 1
+fi
+
+access_point="ap$peer_id"
+gnome-terminal -x sh -c "cd ../src/; java Application $access_point RESTORE $file_path"
