1efbf234b314e5240ec4b2cbae8ded47a0311e80
Pedro Baptista <ze.pedro3622@outlook.pt>
2020-05-12 10:17:15 +0100

    Few script changes

--- lab05/client.sh
+++ lab05/client.sh
@@ -1,6 +1,6 @@
 #!/usr/bin/env bash
 
-OUT_DIR="build/"
+BUILD_DIR="build/"
 
 SSLClient="SSLClient"
 Host="localhost"
@@ -9,24 +9,28 @@ CypherSuite=("TLS_RSA_WITH_AES_128_CBC_SHA" "TLS_DHE_RSA_WITH_AES_128_CBC_SHA")
 
 KeyStore="client.keys"
 KeyStore_PW="123456"
+
 TrustStore="truststore"
 TrustStore_PW="123456"
 
-if [[ "$#" -ne 1 && "$#" -ne 2 && "$#" -ne 3 ]]; then
+nArgs="$#"
+
+if [[ nArgs -lt 1 || nArgs -gt 3 ]]; then
   echo "Usage: client.sh <oper> <opnd>*"
+  echo "Possible <oper>: register | lookup | reset | close"
   exit 1
 fi
 
 if [[ "$#" -eq 1 ]]; then
     java -Djavax.net.ssl.keyStore="$KeyStore" -Djavax.net.ssl.keyStorePassword="$KeyStore_PW" \
         -Djavax.net.ssl.trustStore="$TrustStore" -Djavax.net.ssl.trustStorePassword="$TrustStore_PW" \
-        -cp "$OUT_DIR" "$SSLClient" "$Host" "$Port" "$1" "${CypherSuite[@]}"
+        -cp "$BUILD_DIR" "$SSLClient" "$Host" "$Port" "$1" "${CypherSuite[@]}"
 elif [[ "$#" -eq 2 ]]; then
     java -Djavax.net.ssl.keyStore="$KeyStore" -Djavax.net.ssl.keyStorePassword="$KeyStore_PW" \
         -Djavax.net.ssl.trustStore="$TrustStore" -Djavax.net.ssl.trustStorePassword="$TrustStore_PW" \
-        -cp "$OUT_DIR" "$SSLClient" "$Host" "$Port" "$1" "$2" "${CypherSuite[@]}"
+        -cp "$BUILD_DIR" "$SSLClient" "$Host" "$Port" "$1" "$2" "${CypherSuite[@]}"
 elif [[ "$#" -eq 3 ]]; then
     java -Djavax.net.ssl.keyStore="$KeyStore" -Djavax.net.ssl.keyStorePassword="$KeyStore_PW" \
         -Djavax.net.ssl.trustStore="$TrustStore" -Djavax.net.ssl.trustStorePassword="$TrustStore_PW" \
-        -cp "$OUT_DIR" "$SSLClient" "$Host" "$Port" "$1" "$2" "$3" "${CypherSuite[@]}"
+        -cp "$BUILD_DIR" "$SSLClient" "$Host" "$Port" "$1" "$2" "$3" "${CypherSuite[@]}"
 fi
--- lab05/server.sh
+++ lab05/server.sh
@@ -1,18 +1,17 @@
 #!/usr/bin/env bash
 
-OUT_DIR="build/"
+BUILD_DIR="build/"
 
 SSLServer="SSLServer"
-Port=12345
+ServerPort=12345
 CypherSuite=("TLS_RSA_WITH_AES_128_CBC_SHA" "TLS_DHE_RSA_WITH_AES_128_CBC_SHA")
 
 KeyStore="server.keys"
 KeyStore_PW="123456"
+
 TrustStore="truststore"
 TrustStore_PW="123456"
 
-echo "${@:2}"
-
 java -Djavax.net.ssl.keyStore="$KeyStore" -Djavax.net.ssl.keyStorePassword="$KeyStore_PW" \
      -Djavax.net.ssl.trustStore="$TrustStore" -Djavax.net.ssl.trustStorePassword="$TrustStore_PW" \
-     -cp "$OUT_DIR" "$SSLServer" "$Port" "${CypherSuite[@]}"
+     -cp "$OUT_DIR" "$SSLServer" "$ServerPort" "${CypherSuite[@]}"
