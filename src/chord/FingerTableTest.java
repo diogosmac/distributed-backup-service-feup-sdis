77082eb0715f098a32a03053522d9007282a4c6d
Marantesss <up201706917@fe.up.pt>
2020-05-13 17:23:43 +0100

    Removed unnecessary import

--- src/chord/FingerTableTest.java
+++ src/chord/FingerTableTest.java
@@ -1,6 +1,5 @@
 package chord;
 
-import java.net.InetAddress;
 import java.net.InetSocketAddress;
 
 public class FingerTableTest {
@@ -9,22 +8,22 @@ public class FingerTableTest {
         // create finger table
         int m = 5;
         FingerTable table = new FingerTable(m);
-        table.addNodePair(0, new NodePair<Integer,InetSocketAddress>(4, null));
-        table.addNodePair(1, new NodePair<Integer,InetSocketAddress>(4, null));
-        table.addNodePair(2, new NodePair<Integer,InetSocketAddress>(9, null));
-        table.addNodePair(3, new NodePair<Integer,InetSocketAddress>(9, null));
-        table.addNodePair(4, new NodePair<Integer,InetSocketAddress>(18, null));
+        table.setNodePair(0, new NodePair<Integer,InetSocketAddress>(4, null));
+        table.setNodePair(1, new NodePair<Integer,InetSocketAddress>(4, null));
+        table.setNodePair(2, new NodePair<Integer,InetSocketAddress>(9, null));
+        table.setNodePair(3, new NodePair<Integer,InetSocketAddress>(9, null));
+        table.setNodePair(4, new NodePair<Integer,InetSocketAddress>(18, null));
 
         // lookup
         //Integer result = table.lookup(1, 8);
         //System.out.println(result);
 
         table = new FingerTable(m);
-        table.addNodePair(0, new NodePair<Integer,InetSocketAddress>(1, null));
-        table.addNodePair(1, new NodePair<Integer,InetSocketAddress>(2, null));
-        table.addNodePair(2, new NodePair<Integer,InetSocketAddress>(3, null));
-        table.addNodePair(3, new NodePair<Integer,InetSocketAddress>(4, null));
-        table.addNodePair(4, new NodePair<Integer,InetSocketAddress>(14, null));
+        table.setNodePair(0, new NodePair<Integer,InetSocketAddress>(1, null));
+        table.setNodePair(1, new NodePair<Integer,InetSocketAddress>(2, null));
+        table.setNodePair(2, new NodePair<Integer,InetSocketAddress>(3, null));
+        table.setNodePair(3, new NodePair<Integer,InetSocketAddress>(4, null));
+        table.setNodePair(4, new NodePair<Integer,InetSocketAddress>(14, null));
 
         // lookup
         //result = table.lookup(28, 12);
